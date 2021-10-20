73a718f44d7e60cde90873851003449b086685e1
Julio Neto <julioneto09@gmail.com>
2021-10-20 12:06:14 -0300

    update q7

--- q7.py
+++ q7.py
@@ -8,22 +8,35 @@ a = input('Escreve alguma coisa: ')
 b = input('Escreve alguma coisa: ')
 
 comp_a = len(a)
+
 comp_b = len(b)
 
-print("A primeira coisa escrita foi: '%s', e seu comprimento é %d" %(a, comp_a))
+print("\nA primeira coisa escrita foi: '%s', e seu comprimento é %d" %(a, comp_a))
 print("A segunda coisa escrita foi: '%s', e seu comprimento é %d" %(b, comp_b))
-
+x = comp_a
 a = a.strip().lower()
 b = b.strip().lower()
 
-i = 0
+'''
+Removendo espaços em branco no início e fim das strings, e forçando as duas sentenças a terem apenas caracteres 
+minúsculos, para analisar se o conteúdo/mensagem de ambas é o mesmo.'''
 
+i = 0
 while i < comp_a :
     if a[i] == b[i] :
         i += 1
     else :
+        print ('erro')
         break
 if i == comp_a :
-    print('o conteudo das duas strings é igual')
+    print('\nLogo, o conteúdo das duas strings é igual!')
 else:
-    print('o conteudo das duas strings é diferente')
+    print('\nAssim, o conteúdo das duas strings é diferente!')
+
+
+#Notas mentais
+'''
+Se a primiera string (a verificação é sobre seu comprimento) tiver espaços vazios no inicio, deu erro.
+Apesar da verificação de espaços no vazios no inicio da string, se houver espaços vazios 
+no 'meio' da string, mesmo que a mensagem seja a mesma, o código acusará conteúdo diferente.
+'''
